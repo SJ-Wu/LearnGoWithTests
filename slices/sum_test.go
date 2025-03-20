a92df11325a7d63bdc1cc972601f0b35f37a541e
ShengJu <nickwu0301@gmail.com>
2025-03-20 23:40:20 +0800

    refactor(slices): update Sum function to handle slices of any size - Change `Sum` function parameter from fixed array to slice - Update tests to support dynamic slice sizes

--- slices/sum_test.go
+++ slices/sum_test.go
@@ -5,10 +5,12 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d, want %d given %v", got, want, numbers)
-	}
+	t.Run("Collection of any sizes", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d, want %d given %v", got, want, numbers)
+		}
+	})
 }
