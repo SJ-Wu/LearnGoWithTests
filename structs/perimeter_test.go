ddef94e8f32dd94ea4896bef6fc569705a572bf3
ShengJu <nickwu0301@gmail.com>
2025-03-24 23:15:09 +0800

    refactor(tests): simplify `TestArea` using table-driven tests

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -15,19 +15,17 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-	t.Run("Circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, math.Pi*10.0*10.0)
-	})
-}
-
-func checkArea(t *testing.T, shape Shape, want float64) {
-	got := shape.Area()
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, math.Pi * math.Pow(10, 2)},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f, want %.2f", got, tt.want)
+		}
 	}
 }
