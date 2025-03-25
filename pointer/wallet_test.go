c7ef50869f6ecd7ac1799a8decc72b8a33381634
nick.wu <nick.wu@cp-mail.chippay.com>
2025-03-25 15:35:10 +0800

    feat(wallet): add Wallet struct with Deposit and Balance methods - Implement Bitcoin type with Stringer interface - Add unit tests for Wallet functionalities

---
+++ pointer/wallet_test.go
@@ -0,0 +1,13 @@
+package pointer
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+	got := wallet.Balance()
+	want := Bitcoin(11)
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
